fcc80b13a08e211b944ef5926ab81a2289d169da
BergNovis <jacktwist.ennis@outlook.com>
2019-10-11 19:17:21 +0800

    新增了文件/窗口调试

--- public/js/docwindJs.js
+++ public/js/docwindJs.js
@@ -1,9 +1,22 @@
 $(document).ready(function () {
-   $('img').load(function () {
-       alert("图片已载入！");
-   });
 
+    x = 0;
     $(window).resize(function(){
         $("span[name='ckcount']").text(x+=1);
+    });
+
+    // $('img').load(function () {  //于jQuery1.8版本被移除
+    //     alert("图片已载入！");
+    // });
+
+    // $(window).unload(function(){  //unload() 方法在 jQuery 版本 1.8 中被废弃，在 3.0 版本被移除。
+    //     alert("Goodbye!");
+    // });
+
+    y = 0;
+    $(document).ready(function(){
+        $("div[name='gdcount']").scroll(function(){
+            $("span[name='gdcount']").text(y+=1);
+        });
     });
 });
